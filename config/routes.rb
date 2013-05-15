365dcd38a30b9513052dbf29a1060e25f3d742db
fm2munsh <farhanm995@gmail.com>
2013-05-15 18:13:47 -0400

    Added header

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Due::Application.routes.draw do
-
+	
 	resources :home
-		root :to => 'home#index'
-		match '/' => 'home#index'
+		root :to 		=> 'home#index'
+		match '/' 		=> 'home#index'
 
 	resources :sessions
-		get "logout" => "sessions#destroy"
-		get "login" => "sessions#new"
+		get "logout" 	=> "sessions#destroy"
+		get "login" 	=> "sessions#new"
 
 	resources :users
-		get "signup" => "users#new"
+		get "signup" 	=> "users#new"
+
+	resources :groups
+  		get "groups" 	=> "groups#index"
 
 end
