9f237213a21c29424be154c4606d065e109b1403
Aiden Lee <aiden.jihun.lee@gmail.com>
2013-05-15 18:44:16 -0400

    Index shows list of existing groups.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @group = Group.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130515214256) do
 
-  create_table "admins", :force => true do |t|
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-  end
-
   create_table "groups", :force => true do |t|
     t.string   "name"
     t.string   "password"
