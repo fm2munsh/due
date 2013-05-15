80528c79409b9af7e9a280ca288e8f19dd6b16e0
fm2munsh <farhanm995@gmail.com>
2013-05-15 15:10:51 -0400

    Created user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130515184730) do
+ActiveRecord::Schema.define(:version => 20130515191025) do
 
   create_table "admins", :force => true do |t|
     t.string   "email"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(:version => 20130515184730) do
     t.integer  "post_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
