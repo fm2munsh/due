bc4e825704f84f4a1da62e2aecc64ba6ce81f5f0
Daniel Schwarz <danschwarz00@gmail.com>
2013-05-15 15:16:39 -0400

    added join table and relations

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130515184730) do
+ActiveRecord::Schema.define(:version => 20130515190035) do
 
   create_table "admins", :force => true do |t|
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+  end
+
+  create_table "admins_groups", :id => false, :force => true do |t|
+    t.integer "admin_id"
+    t.integer "group_id"
   end
 
   create_table "groups", :force => true do |t|
