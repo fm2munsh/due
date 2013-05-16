dfe70d9301b7209221101b936c3fd20901c8edaf
fm2munsh <farhanm995@gmail.com>
2013-05-15 22:05:51 -0400

    Merge

--- config/routes.rb
+++ config/routes.rb
@@ -10,18 +10,14 @@ Due::Application.routes.draw do
 	end
 
 	resources :users
-<<<<<<< HEAD
 	resources :sessions, only: [:new, :create, :destroy]
-	resources :posts, only: [:new, :delete]
 
-=======
-	resources :sessions # not sure why?
 	resources :posts, only: [:new, :delete] do
 		collection do
 			match "/ajaxnew" => "posts#ajaxnew"
 		end
 	end
->>>>>>> d351cb8ca833c66f87ce05ea2e77d1debbdb1cbf
+	
 	match "signup" => "users#new", as: "signup"
 	match "logout" => "sessions#destroy", as: "logout"
 	match "login" => "sessions#new", as: "login"
